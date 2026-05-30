e062f2cc0eaeabb6d453ddcd33bf609bb8f5abf3
mactuhau <tuhaumac2003@gmail.com>
2026-05-30 15:31:03 +0700

    feat(fe): add register store and update auth services to include user registration functionality

--- fe/src/features/auth/services/index.ts
+++ fe/src/features/auth/services/index.ts
@@ -5,4 +5,5 @@ export {
   getRememberedEmail,
   normalizeLoginResponse,
   persistRememberedEmail,
+  registerUser,
 } from './authService'
--- fe/src/features/auth/stores/index.ts
+++ fe/src/features/auth/stores/index.ts
@@ -1,1 +1,2 @@
 export { useLoginStore } from './loginStore'
+export { useRegisterStore } from './registerStore'
